2fa73b9c8053eed254757493e2358c81a867b629
Samuel Goldman <6364482+samgoldman@users.noreply.github.com>
2020-04-24 20:41:21 -0600

    Update aggregate_by_agency_country_month.sql
    Remove extraneous group by element

--- sql/aggregate_by_agency_country_month.sql
+++ sql/aggregate_by_agency_country_month.sql
@@ -2,4 +2,4 @@ SELECT SUM(ts.federal_action_obligation) as sum, ts.awarding_agency_name, SUBSTR
 FROM transaction_fpds ts
     JOIN ref_country_code cc ON (cc.country_name = ts.legal_entity_country_code or cc.country_code = ts.legal_entity_country_code)
 WHERE ts.legal_entity_country_code != 'NAN'
-GROUP BY ts.awarding_agency_name, SUBSTRING(ts.period_of_performance_star, 1, 7), cc.country_code, SUBSTRING(ts.period_of_performance_star, 1, 4) , SUBSTRING(ts.period_of_performance_star, 6, 2),  ts.awarding_agency_name;
+GROUP BY ts.awarding_agency_name, cc.country_code, SUBSTRING(ts.period_of_performance_star, 1, 4) , SUBSTRING(ts.period_of_performance_star, 6, 2),  ts.awarding_agency_name;
