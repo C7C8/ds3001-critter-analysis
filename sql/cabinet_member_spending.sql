3de9f1ae9e9a943cfcbe4c5468f311417451001b
Nick Krichevsky <nick@ollien.com>
2020-04-27 19:32:43 -0400

    Update cabinet spending data to only account for respective departments

--- sql/cabinet_member_spending.sql
+++ sql/cabinet_member_spending.sql
@@ -1,4 +1,4 @@
-SELESELECT
+SELECT
 	cb."Department",
 	cb."President",
 	cb."Nominee",
@@ -8,7 +8,9 @@ SELESELECT
 	SUM(CASE WHEN cc.country_code = 'USA' THEN ts.federal_action_obligation ELSE 0 END) AS domestic_spending,
 	SUM(CASE WHEN cc.country_code NOT IN ('USA', 'NAN') THEN ts.federal_action_obligation ELSE 0 END) AS foreign_spending
 	FROM "Cabinet Data - presidents" cb
-	JOIN (Select period_of_performance_star::date, federal_action_obligation, legal_entity_country_code FROM transaction_fpds) ts
-		ON ts.period_of_performance_star BETWEEN cb."Confirmed" AND cb."End date" JOIN ref_country_code cc
+	JOIN (Select period_of_performance_star::date, federal_action_obligation, legal_entity_country_code, awarding_agency_name FROM transaction_fpds) ts
+		ON ts.period_of_performance_star BETWEEN cb."Confirmed" AND cb."End date"
+		AND ts.awarding_agency_name = cb."Department"
+	JOIN ref_country_code cc
 		ON (cc.country_name = ts.legal_entity_country_code or cc.country_code = ts.legal_entity_country_code)
 	GROUP BY cb."Department", cb."President", cb."Nominee", cb."Votes For", cb."Votes Against", cb."Days";
