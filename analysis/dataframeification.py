d64c6e825fa2d952a86c43c6be59e09727518704
Sourec <tothemyers@gmail.com>
2020-04-27 15:55:34 -0400

    Add some CSV to dataframe scripts (w/cleaning)

---
+++ analysis/dataframeification.py
@@ -0,0 +1,43 @@
+"""Script for loading all the CSVs and converting them to dataframes with proper data types for each column."""
+
+import pandas as pd
+import re
+
+acronym_splitter_regex = "^(.*)(\\(\\w+\\))$"
+
+
+# Aggregate_federal_country_year_agency.csv
+df = pd.read_csv("data/csv/Aggregate_federal_country_year_agency_.csv", index_col=False)
+# Split agency names from their acronyms, may make sorting easier
+tmp = df["agency"].str.extract(acronym_splitter_regex)
+df["agency"] = tmp[0].str.strip()
+df["agency_acr"] = tmp[1].str[1:-1]
+
+df = df.dropna(axis=0)\
+	.sort_values(["year", "agency"])\
+	.reset_index(drop=True)\
+	.to_pickle("data/df/aggregate_federal_country_year_agency.pkl.gz")
+
+
+# Aggregate_federal_spending_country_month_agency
+df = pd.read_csv("data/csv/Aggregate_federal_spending_country_month_agency.csv")
+tmp = df["agency"].str.extract(acronym_splitter_regex)
+df["agency"] = tmp[0].str.strip()
+df["agency_acr"] = tmp[1].str[1:-1]
+
+df = df.dropna(axis=0)\
+	.sort_values(["year", "month", "agency"])\
+	.reset_index(drop=True)\
+	.to_pickle("data/df/aggregate_federal_country_month_agency.pkl.gz")
+
+
+# foreign_spending_by_agency_year_country_all
+df = pd.read_csv("data/csv/foreign_spending_by_agency_year_country_all.csv")
+tmp = df["agency"].str.extract(acronym_splitter_regex)
+df["agency"] = tmp[0].str.strip()
+df["agency_acr"] = tmp[1].str[1:-1]
+
+df = df.dropna(axis=0) \
+	.sort_values(["year", "agency"]) \
+	.reset_index(drop=True) \
+	.to_pickle("data/df/foreign_spending_by_agency_year_country_all.pkl.gz")
