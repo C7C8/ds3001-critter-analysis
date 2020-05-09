25566c401feaa85cafcde25ae85a17615747e8e5
samgoldman <sgoldman216@gmail.com>
2020-05-08 21:19:06 -0600

    Updated model

--- d3/javascript/model.js
+++ d3/javascript/model.js
@@ -1,110 +1,54 @@
 let agencies = null;
 let countries = null;
+let initial_data = null;
 const features = {};
 let forest = null;
 
 Promise.all([d3.csv(`../data/models/rf_trees/meta/agencies.csv`),
-	d3.csv('../data/models/rf_trees/meta/2018_january.csv'),
+	d3.csv('../data/models/rf_trees/meta/2014_september.csv'),
 	d3.csv(`../data/models/rf_trees/meta/countries.csv`)]).then(async data => {
-	data[1].sort((a,b ) => ((a['country'] < b['country']) ? -1 : ((a['country'] > b['country']) ? 1 : 0)))
-	const rows = data[1].length + 1;
-	const cols = data[0].length + 1;
-
 	agencies = data[0];
+	initial_data = data[1];
 	countries = data[2];
 
-	const body = document.getElementsByTagName('body')[0];
-	const tbl = document.createElement('table');
-	tbl.style.width = '100%';
-	tbl.setAttribute('border', '1');
-	const tbdy = document.createElement('tbody');
-	for (let i = 0; i < rows; i++) {
-		const tr = document.createElement('tr');
-		for (let j = 0; j < cols; j++) {
-			const td = document.createElement('td');
-			if (i === 0) {
-				if (j === 0) td.innerText = 'Country/Agency'
-				else if (j <= data[0].length) td.innerText = data[0][j-1]['agency']
-			} else if (j === 0 && i >= 1) td.innerText = data[1][i - 1]['country']
-			else {
-				const input = document.createElement('input');
-				input.setAttribute('type', 'number');
-				if (j <= data[0].length) input.id = data[0][j-1]['agency'];
-				input.value = Math.round(data[1][i-1][data[0][j-1]['agency']]);
-				input.id += '-' + data[1][i-1]['country'];
-				input.onchange = predict;
-				td.appendChild(input)
-			}
-			tr.appendChild(td)
-		}
-		tbdy.appendChild(tr);
-	}
-	tbl.appendChild(tbdy);
-	body.appendChild(tbl);
+	data[1].forEach(d => {
+		document.body.append(document.createElement('br'));
+		const label = document.createElement('p');
+		label.innerText = d['feature'];
+		const input = document.createElement('input');
+		input.value = d['value'];
+		input.setAttribute('id', d['feature'])
+		input.onchange = predict;
+		document.body.append(label);
+		document.body.append(input);
+	});
+
 	forest = await RandomForest.loadForest(185);
 	predict();
 });
 
+const reload = () => {
+	const value = document.querySelector('#file_selector').value;
+	Promise.all([d3.csv(`../data/models/rf_trees/meta/${value}.csv`)]).then(async data => {
+		data[0].forEach(d => {
+			const input = document.querySelector(`#${d['feature']}`);
+			input.value = d['value'];
+		});
+		predict();
+	});
+}
+
 const predict = () => {
 	const results = forest.predict(calculateFeatures());
-
+	console.log(results);
 	document.querySelector('#prediction_label').innerText = `By a ${Math.max(results[0], results[1])}-${Math.min(results[0], results[1])} margin, our forest thinks your administration would likely be ${results[0] > results[1] ? 'Republican' : 'Democrat'}.`;
 }
 
 const calculateFeatures = () => {
-	// Expect 3456 features
-	const features = {
-		'total': 0,
-		'total_foreign': 0
-	}
-
-	agencies.forEach(agency => {
-		features[`total_${agency['agency']}`] = 0;
-		features[`total_foreign_${agency['agency']}`] = 0;
+	const features = {};
+	initial_data.forEach(k => {
+		features[k.feature] = parseFloat(document.querySelector(`#${k.feature}`).value);
 	});
-
-	countries.forEach(country => {
-		features[`total_to_${country['country']}`] = 0;
-	});
-
-	agencies.forEach(agency => {
-		countries.forEach(country => {
-			const value = parseInt(document.querySelector(`#${agency['agency']}-${country['country']}`).value);
-
-			features['total'] += value;
-			features[`total_${agency['agency']}`] += value;
-			features[`total_to_${country['country']}`] += value;
-
-			if ('USA' !== country['country']) {
-				features[`total_foreign_${agency['agency']}`] += value;
-				features['total_foreign'] += value;
-			}
-		});
-	});
-
-	features['pct_foreign'] = features['total_foreign'] / features['total'];
-
-	delete features['total_foreign'];
-
-	agencies.forEach(agency => {
-		features[`pct_foreign_${agency['agency']}`] = features[`total_foreign_${agency['agency']}`] / features[`total_${agency['agency']}`];
-		delete features[`total_foreign_${agency['agency']}`];
-	});
-
-	countries.forEach(country => {
-		features[`pct_total_to_${country['country']}`] = features[`total_to_${country['country']}`] / features['total'];
-	});
-
-	agencies.forEach(agency => {
-		countries.forEach(country => {
-			features[`pct_${agency['agency']}_total_to_${country['country']}`] = features[`total_to_${country['country']}`] / features[`total_${agency['agency']}`];
-			delete features[`total_to_${country['country']}`];
-		});
-	});
-
-	Object.keys(features).forEach(key => {
-		if (isNaN(features[key])) features[key] = 0;
-	})
 
 	return features;
 }
--- scripts/generate-starter-month-file.py
+++ scripts/generate-starter-month-file.py
@@ -6,18 +6,32 @@ def main():
 
     data = data.fillna(0)
 
-    month_data = data.loc[2018.0]
+    month_data = data.loc[2014.6666666666667]
 
     countries = pd.read_csv('../data/models/rf_trees/meta/countries.csv')['country'].to_list()
     agencies = pd.read_csv('../data/models/rf_trees/meta/agencies.csv')['agency'].to_list()
 
+    agencies.remove('oth')
+
     sample_month = pd.DataFrame(columns=agencies, index=countries)
 
-    for country in countries:
-        for agency in agencies:
-            sample_month.at[country, agency] = month_data['total_{}'.format(agency)] * month_data['pct_{}_total_to_{}'.format(agency, country)]
+    other_agency_total = month_data['total']
+    for agency in agencies:
+        other_agency_total -= month_data['total_{}'.format(agency)]
 
-    sample_month.to_csv('../data/models/rf_trees/meta/2018_january.csv')
+    for country in countries:
+        country_total_from_other_agencies = month_data['pct_total_to_{}'.format(country)] * month_data['total']
+        for agency in agencies:
+            country_total_from_other_agencies -= month_data['total_{}'.format(agency)] * month_data['pct_{}_total_to_{}'.format(agency, country)]
+            sample_month.at[country, agency] = month_data['total_{}'.format(agency)] * month_data['pct_{}_total_to_{}'.format(agency, country)]
+        sample_month.at[country, 'oth'] = country_total_from_other_agencies
+
+    for agency in agencies:
+        print('{} total: {}'.format(agency, month_data['total_{}'.format(agency)]))
+
+    print('Total: {}'.format(month_data['total']))
+
+    sample_month.to_csv('../data/models/rf_trees/meta/2014_august.csv')
 
 
 if __name__ == "__main__":
